abe3956cea98df3edf76a94b9b2eb621f7da1599
JayeshW <jayeshwankhee28@gmail.com>
2024-02-15 02:58:21 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col mx-[40px]" >
+      <HeroSection />
     </main>
   );
 }
