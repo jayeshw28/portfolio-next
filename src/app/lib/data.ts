a93ef952290b521f677ff99b298e79e88212496d
JayeshW <jayeshwankhee28@gmail.com>
2024-02-18 03:02:30 +0530

    about layout added

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,7 +1,4 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+
 
 export const links = [
   {
@@ -30,13 +27,89 @@ export const links = [
   },
 ] as const;
 
+export const media_links = [
+  {
+    name: "Instagram",
+    img:"",
+    link: "",
+  },
+  {
+    name: "Github",
+    img:"",
+    link: "",
+  },
+  {
+    name: "Linkedin",
+    img:"",
+    link: "",
+  },
+  {
+    name: "Behance",
+    img:"",
+    link: "",
+  },
+  {
+    name: "facebook",
+    img:"",
+    link: "",
+  },
+] as const;
+
+export const stack = [
+  {
+    name: "React",
+    img:"",
+  },
+  {
+    name: "Next.js",
+    img:"",
+  },
+  {
+    name: "Tailwind",
+    img:"",
+  },
+  {
+    name: "MongoDB",
+    img:"",
+  },
+  {
+    name: "Redux",
+    img:"",
+  },
+  {
+    name: "Apollo",
+    img:"",
+  },
+  {
+    name: "Express",
+    img:"",
+  },
+  {
+    name: "PostgreSQL",
+    img:"",
+  },
+  {
+    name: "Python",
+    img:"",
+  },
+  {
+    name: "Django",
+    img:"",
+  },
+  {
+    name: "Framer Motion",
+    img:"",
+  },
+] as const;
+
+
 export const experiencesData = [
   {
     title: "Graduated bootcamp",
     location: "Miami, FL",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
+    icon: '🎓',
     date: "2019",
   },
   {
@@ -44,7 +117,7 @@ export const experiencesData = [
     location: "Orlando, FL",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
+    icon: '🎨',
     date: "2019 - 2021",
   },
   {
@@ -52,7 +125,7 @@ export const experiencesData = [
     location: "Houston, TX",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
+    icon: '🚀',
     date: "2021 - present",
   },
 ] as const;
