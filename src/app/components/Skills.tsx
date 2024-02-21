8e3bf9255742eebbb6ad22f598180a2e3f627161
JayeshW <jayeshwankhee28@gmail.com>
2024-02-21 08:05:12 +0530

    skills section responsive added

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -3,8 +3,8 @@ import { languagesData, skillsData, toolsData } from '../lib/data'
 
 const Skills = () => {
   return (
-    <div className='w-full h-screen flex my-5'>
-        <div className='w-1/2 bg-[#B9A0FF] text-[#003F30] p-10 flex flex-col justify-between font-[ReplicaPro-Light] text-xl'>
+    <div className='w-full h-fit lg:h-screen lg:flex my-5'>
+        <div className='w-full lg:w-1/2 bg-[#B9A0FF] text-[#003F30] p-10 flex flex-col justify-between font-[ReplicaPro-Light] text-xl'>
             <div>
                 <p className='font-[Replica-Bold] text-5xl mb-6'>Skills</p>
                 {
@@ -30,10 +30,10 @@ const Skills = () => {
                     ))
                 }
             </div>
-            <p className='font-[Replica-Bold] text-8xl justify-self-end'>*</p>
+            <p className='font-[Replica-Bold] text-8xl lg:self-start self-end'>*</p>
         </div>
 
-        <div className='w-1/2 bg-[#003F30] text-[#B9A0FF] p-10 font-[ReplicaPro-Light] text-xl'>
+        <div className='w-full lg:w-1/2 bg-[#003F30] text-[#B9A0FF] p-10 font-[ReplicaPro-Light] text-xl'>
             <div className='flex flex-row justify-between font-[Replica-Bold] text-5xl mb-6'>
                 <p>Tools</p>
                 <p className='font-[ReplicaPro-Light] text-6xl'>↗</p>
