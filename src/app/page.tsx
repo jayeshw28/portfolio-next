914489f379d5e39486cc5063eb96eef1b62fc36e
JayeshW <jayeshwankhee28@gmail.com>
2024-02-23 18:45:41 +0530

    project section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Project from "./components/Project";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col" >
-      <div className="mx-[40px]">
+      <div className="mx-10">
         <HeroSection/>
-        <About/>
+        {/* <About/> */}
       </div>
       <Skills/>
+      <Project/>
     </main>
   );
 }
