ea8db2bab99cddeb1a85a5e7b0fb05b295512697
JAYESH WANKHEDE <105209034+jayeshw28@users.noreply.github.com>
2025-03-20 22:24:59 +0530

    Update resume link

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -62,7 +62,7 @@ const HeroSection = () => {
       <div className='text-[#B9A0FF] p-4 overflow-y-auto scroll-smooth scrollbar hidden lg:block lg:col-span-1 lg:row-span-2 bg-[#19192C] border-all'>
         <p className='text-center'>Hey there! Compy is monitoring mouse and keyboard activity.</p>
       </div>
-      <a href='https://drive.google.com/file/d/1dYjGGMpR-etsCm3j0iS7V9l3unl4diNG/view?usp=sharing' target='_blank' className='group preserve-3d cursor-pointer flex flex-col justify-between p-6 col-span-2 row-span-1 lg:col-span-2 lg:row-span-2 font-[ReplicaPro-Light] font-bold text-xl lg:text-3xl text-[#003F30] bg-[#C3FFB2] border-all hover:shadow-[-6px_6px_0px_#000000] hover:translate-x-[6px] hover:translate-y-[-6px] transition-[cubic-bezier(.31,.5,.86,.67)] duration-[0.25s]'>
+      <a href='https://drive.google.com/file/d/1eH1b136pkACOJYmomwmurBSFkMuO0HL5/view?usp=sharing' target='_blank' className='group preserve-3d cursor-pointer flex flex-col justify-between p-6 col-span-2 row-span-1 lg:col-span-2 lg:row-span-2 font-[ReplicaPro-Light] font-bold text-xl lg:text-3xl text-[#003F30] bg-[#C3FFB2] border-all hover:shadow-[-6px_6px_0px_#000000] hover:translate-x-[6px] hover:translate-y-[-6px] transition-[cubic-bezier(.31,.5,.86,.67)] duration-[0.25s]'>
         <p>Resume</p>
         <div className='flex justify-end'>
           <button className='border border-[#003F30] px-2 py-1 rounded-[6px]'>
